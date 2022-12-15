a55399cc54390f4a345900e09d8a6384a47cbdc4
Savio Henrique <savio.c.mendes@gmail.com>
2022-12-15 15:14:06 -0300

    Updated study_lesson.py

--- Sem5/study_lesson.py
+++ Sem5/study_lesson.py
@@ -1,12 +1,52 @@
+def sort_text(lista):
+    ordem = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    final = []
+    for i in ordem:
+        if i in lista:
+            for letra in lista:
+                if letra in i:
+                    final.append(letra)
+    return final
+
+def dicio_transform(lista):
+    dicio={}
+    for letra in lista:
+        if letra not in dicio:
+            dicio[letra] = 1
+        else:
+            dicio[letra] += 1
+    return dicio
+
 def busca(lista, valor_buscado):
-    valores_nao_existentes = []
+    if isinstance(lista,str):
+        lista = dicio_transform(lista)
     for i in valor_buscado:
         if i not in lista:
-            return "You died!"
+            return None
+        else:
+            lista[i] -= 1
+            if lista[i] == 0:
+                lista.pop(i)
 
-    return valores_nao_existentes
+    return lista
 
 def main():
     qtd_testes = int(input())
     for x in range(qtd_testes):
         plano = input()
+        for i in range(3):
+            buscado = input()
+            if plano is not None:
+                plano = busca(plano,buscado)
+        if plano is not None and len(plano) == 0:
+            print("It's in the box!")
+        elif plano is not None:
+            print("Bora ralar:", end=" ")
+            plano = sort_text(plano)
+            for l in plano:
+                print(l, end="")
+            print()
+        else:
+            print("You died!")
+
+main()
